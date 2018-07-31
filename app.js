bfb510cbb6818ab468c0eeec16d28dcda5d65154
Sam <sammintz1993@gmail.com>
2018-07-31 11:28:38 -0500

    hide dice before game starts

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ GAME RULES:
 // Global vars
 let scores = [0,0];
 let roundScore = 0;
-let activePLayer = 0; 
+let activePlayer = 0; 
 
 let dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice, "<--random number");
 
-document.querySelector("#score-0").textContent = dice;
+document.querySelector("#current-" + activePlayer).textContent = dice;
 
+document.querySelector(".dice").style.display = "none";
 
 
 
