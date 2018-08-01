1c5b7d6c3b0cedcca335103ee5eb91247f03f2b7
Sam <sammintz1993@gmail.com>
2018-08-01 10:32:11 -0500

    set all values to 0

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ let activePlayer = 0;
 
 // document.querySelector(".dice").style.display = "none";
 
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
 // callback function that is called by another function. this is being called by eventlistener function
 // or write anoynomous function. no name used once
@@ -33,7 +37,5 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
 	let diceDOM = document.querySelector(".dice");
 	diceDOM.style.display = "block";
 	diceDOM.src = "dice-" + dice + ".png"
-
-
 	// 3. Update the round score IF the rolled number was NOT a 1
 });
