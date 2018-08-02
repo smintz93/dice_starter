5cac329f4a95b50221618c6e16c658d0bf32c1fb
Sam <sammintz1993@gmail.com>
2018-08-02 11:08:08 -0500

    active player score gets added based n dice roll

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ let activePlayer = 0;
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 
 // document.querySelector(".dice").style.display = "none";
+// get element by id a bit faster than qs.
 
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
@@ -38,4 +39,13 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
 	diceDOM.style.display = "block";
 	diceDOM.src = "dice-" + dice + ".png"
 	// 3. Update the round score IF the rolled number was NOT a 1
+	if (dice !== 1){
+		// add score
+		roundScore += dice
+		document.querySelector("#current-" + activePlayer).textContent = roundScore
+
+	} else {
+		// next player 
+	}
 });
+
